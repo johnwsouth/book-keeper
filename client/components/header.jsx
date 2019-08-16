21cd6830a30455c3386026dffa5360dee4d69d60
johnwsouth <im.john.south@gmail.com>
2019-08-16 22:56:29 +0000

    complete view all grades feature

---
+++ client/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <div className="header-container d-inline-flex justify-content-center">
+      <div className="header">Student Grade Table</div>
+    </div>
+  );
+}
+
+export default Header;
