f7678067061a41cba1bddf3c69cd318e0c92bf92
johnwsouth <im.john.south@gmail.com>
2019-08-19 18:14:43 +0000

    complete delete-a-grade

--- client/components/grades.jsx
+++ client/components/grades.jsx
@@ -6,6 +6,8 @@ function Grade(props) {
       <td>{props.grade.name}</td>
       <td>{props.grade.course}</td>
       <td>{props.grade.grade}</td>
+      {/* <button type="cancel" className="btn btn-secondary ml-3" onClick={this.handleCancel}>Cancel</button> */}
+      <td className ="operation-cell"><button data-key={props.grade.id} className = "btn btn-secondary" onClick={props.deleteGrade}>Delete</button></td>
 
     </tr>);
 }
@@ -18,12 +20,13 @@ function GradeTable(props) {
           <th scope="col">Student Name</th>
           <th scope="col">Course </th>
           <th scope="col">Grade</th>
+          <th scope="col">Operation</th>
 
         </tr>
       </thead>
       <tbody>
         {props.grades.map(grade => {
-          return <Grade key= {grade.id} grade={grade} />;
+          return <Grade key= {grade.id} grade={grade} deleteGrade={props.deleteGrade}/>;
 
         })}
       </tbody>
