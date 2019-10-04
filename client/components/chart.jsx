d89d8ef45dbd9ebac58ba1df10fa29c2513335ba
johnwsouth <im.john.south@gmail.com>
2019-10-04 03:17:30 +0000

    set max character count to graph names changed price input to indicate decimal dollar value

--- client/components/chart.jsx
+++ client/components/chart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VictoryBar, VictoryAxis, VictoryChart } from 'victory';
+import { VictoryLabel, VictoryBar, VictoryAxis, VictoryChart } from 'victory';
 
 export default class Chart extends React.Component {
   constructor(props) {
@@ -7,16 +7,55 @@ export default class Chart extends React.Component {
     this.state = {
       chartState: null
     };
-    this.dataRotation = this.dataRotation.bind(this);
+    this.dataEntryTotal = this.dataEntryTotal.bind(this);
+    this.dataEntryPricePerUnit = this.dataEntryPricePerUnit.bind(this);
+
   }
 
-  dataRotation() {
+  dataEntryTotal() {
     const data = [];
     for (var i = 0; i < this.props.entries.length; i++) {
+      var entryAbbrevNameIndex;
+      if (this.props.entries[i].entryName.indexOf(' ') > 1) {
+        if (this.props.entries[i].entryName.indexOf(' ') > 4) {
+          entryAbbrevNameIndex = 6;
+        } else {
+          entryAbbrevNameIndex = this.props.entries[i].entryName.indexOf(' ') + 2;
+        }
+      } else if (this.props.entries[i].entryName.length <= 6) {
+        entryAbbrevNameIndex = this.props.entries[i].entryName.length;
+      } else if (this.props.entries[i].entryName.length > 6) {
+        entryAbbrevNameIndex = 6;
+      }
+
       data.push({
-        'Entries': this.props.entries[i].entryName,
+        'Entries': this.props.entries[i].entryName.substring(0, entryAbbrevNameIndex),
         'Entry Totals': parseFloat(this.props.entries[i].entryPrice * this.props.entries[i].entryUnits / 100),
         'label': parseFloat(this.props.entries[i].entryPrice * this.props.entries[i].entryUnits / 100)
+      });
+    }
+    return data;
+  }
+
+  dataEntryPricePerUnit() {
+    const data = [];
+    for (var i = 0; i < this.props.entries.length; i++) {
+      var entryAbbrevNameIndex;
+      if (this.props.entries[i].entryName.indexOf(' ') > 1) {
+        if (this.props.entries[i].entryName.indexOf(' ') > 4) {
+          entryAbbrevNameIndex = 6;
+        } else {
+          entryAbbrevNameIndex = this.props.entries[i].entryName.indexOf(' ') + 2;
+        }
+      } else if (this.props.entries[i].entryName.length <= 6) {
+        entryAbbrevNameIndex = this.props.entries[i].entryName.length;
+      } else if (this.props.entries[i].entryName.length > 6) {
+        entryAbbrevNameIndex = 6;
+      }
+      data.push({
+        'Entries': this.props.entries[i].entryName.substring(0, entryAbbrevNameIndex),
+        'Entry Totals': parseFloat(this.props.entries[i].entryPrice / 100),
+        'label': parseFloat(this.props.entries[i].entryPrice / 100)
       });
     }
     return data;
@@ -25,17 +64,19 @@ export default class Chart extends React.Component {
   render() {
 
     if (this.props.entries[0] !== undefined) {
-      var data = this.dataRotation();
+      var dataEntryTotal = this.dataEntryTotal();
+      var dataEntryPricePerUnit = this.dataEntryPricePerUnit();
+
       return (
         <>
 
           <VictoryChart
 
             domainPadding={{ x: 20 }}
-            style={{ parent: { maxWidth: '30%', display: 'inline-block', padding: '30px' } }}
-          >
-            <VictoryBar style={{ parent: { maxWidth: '30%' } }}
-              data={data}
+            style={{ parent: { maxWidth: '40%', display: 'inline-block', 'paddingLeft': '50px' } }}>
+            <VictoryLabel text="Price per Entry" x={225} y={5} textAnchor="middle" />
+            <VictoryBar style={{ parent: { maxWidth: '50%' } }}
+              data={dataEntryTotal}
               // data accessor for x values
               x='Entries'
               // data accessor for y values
@@ -49,9 +90,9 @@ export default class Chart extends React.Component {
               }}
             />
             <VictoryAxis dependentAxis
-              label="Entry Totals"
+              label="Entry Total Cost"
               style={{
-                axisLabel: { padding: 40 }
+                axisLabel: { padding: 35 }
               }}
             />
           </ VictoryChart>
@@ -59,10 +100,10 @@ export default class Chart extends React.Component {
           <VictoryChart
 
             domainPadding={{ x: 20 }}
-            style={{ parent: { maxWidth: '30%', display: 'inline-block' } }}
-          >
-            <VictoryBar style={{ parent: { maxWidth: '30%' } }}
-              data={data}
+            style={{ parent: { maxWidth: '38%', display: 'inline-block' } }}>
+            <VictoryLabel text="Price per Unit" x={225} y={5} textAnchor="middle" />
+            <VictoryBar style={{ parent: { maxWidth: '50%' } }}
+              data={dataEntryPricePerUnit}
               // data accessor for x values
               x='Entries'
               // data accessor for y values
@@ -76,9 +117,9 @@ export default class Chart extends React.Component {
               }}
             />
             <VictoryAxis dependentAxis
-              label="Entry Totals"
+              label="Entry Price Per Unit"
               style={{
-                axisLabel: { padding: 40 }
+                axisLabel: { padding: 35 }
               }}
             />
           </ VictoryChart>
