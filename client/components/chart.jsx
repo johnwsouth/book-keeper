df9a08d95c2d31ccca75eaf4c93f584ffb419e8d
johnwsouth <im.john.south@gmail.com>
2019-10-04 04:42:16 +0000

    added zoom to charts on the x axis. can zoom and pan. Set y axis limiter

--- client/components/chart.jsx
+++ client/components/chart.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import { VictoryLabel, VictoryBar, VictoryAxis, VictoryChart } from 'victory';
+import { VictoryLabel, VictoryBar, VictoryAxis, VictoryChart, VictoryZoomContainer } from 'victory';
 
 export default class Chart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      chartState: null
+      zoomedXDomain: 0
     };
     this.dataEntryTotal = this.dataEntryTotal.bind(this);
     this.dataEntryPricePerUnit = this.dataEntryPricePerUnit.bind(this);
 
   }
 
+  onDomainChange(domain) {
+    this.setState({
+      zoomedXDomain: domain.x
+    });
+  }
+
   dataEntryTotal() {
     const data = [];
+
     for (var i = 0; i < this.props.entries.length; i++) {
       var entryAbbrevNameIndex;
       if (this.props.entries[i].entryName.indexOf(' ') > 1) {
@@ -33,8 +40,10 @@ export default class Chart extends React.Component {
         'Entry Totals': parseFloat(this.props.entries[i].entryPrice * this.props.entries[i].entryUnits / 100),
         'label': parseFloat(this.props.entries[i].entryPrice * this.props.entries[i].entryUnits / 100)
       });
+
     }
     return data;
+
   }
 
   dataEntryPricePerUnit() {
@@ -67,38 +76,50 @@ export default class Chart extends React.Component {
       var dataEntryTotal = this.dataEntryTotal();
       var dataEntryPricePerUnit = this.dataEntryPricePerUnit();
 
+      var largestEntryTotal = 0;
+      for (var totalsIndex = 0; totalsIndex < dataEntryTotal.length; totalsIndex++) {
+        if (dataEntryTotal[totalsIndex]['Entry Totals'] > largestEntryTotal) {
+          largestEntryTotal = dataEntryTotal[totalsIndex]['Entry Totals'];
+        }
+      }
+
+      var largestEntryUnit = 0;
+      for (var unitsIndex = 0; unitsIndex < dataEntryPricePerUnit.length; unitsIndex++) {
+        if (dataEntryPricePerUnit[unitsIndex]['Entry Totals'] > largestEntryUnit) {
+          largestEntryUnit = dataEntryPricePerUnit[unitsIndex]['Entry Totals'];
+        }
+      }
       return (
         <>
+            <VictoryChart
+              containerComponent={<VictoryZoomContainer zoomDimension="x" zoomDomain={{ y: [0, largestEntryTotal * 1.2] }}/>}
+              domainPadding={{ x: 20 }}
+              style={{ parent: { maxWidth: '35%', display: 'inline-block', 'paddingLeft': '50px' } }}>
+              <VictoryLabel text="Price per Entry" x={225} y={5} textAnchor="middle" />
+              <VictoryBar style={{ parent: { maxWidth: '50%' } }}
+                data={dataEntryTotal}
+                // data accessor for x values
+                x='Entries'
+                // data accessor for y values
+                y='Entry Totals'
+                animate={{ duration: 2000 }}
+              />
+              <VictoryAxis
+                label="Entries"
+                style={{
+                  axisLabel: { padding: 30 }
+                }}
+              />
+              <VictoryAxis dependentAxis
+                label="Entry Total Cost"
+                style={{
+                  axisLabel: { padding: 35 }
+                }}
+              />
+            </ VictoryChart>
 
           <VictoryChart
-
-            domainPadding={{ x: 20 }}
-            style={{ parent: { maxWidth: '40%', display: 'inline-block', 'paddingLeft': '50px' } }}>
-            <VictoryLabel text="Price per Entry" x={225} y={5} textAnchor="middle" />
-            <VictoryBar style={{ parent: { maxWidth: '50%' } }}
-              data={dataEntryTotal}
-              // data accessor for x values
-              x='Entries'
-              // data accessor for y values
-              y='Entry Totals'
-              animate={{ duration: 2000 }}
-            />
-            <VictoryAxis
-              label="Entries"
-              style={{
-                axisLabel: { padding: 30 }
-              }}
-            />
-            <VictoryAxis dependentAxis
-              label="Entry Total Cost"
-              style={{
-                axisLabel: { padding: 35 }
-              }}
-            />
-          </ VictoryChart>
-
-          <VictoryChart
-
+            containerComponent={<VictoryZoomContainer zoomDimension="x" zoomDomain={{ y: [0, largestEntryUnit * 1.2] }} />}
             domainPadding={{ x: 20 }}
             style={{ parent: { maxWidth: '38%', display: 'inline-block' } }}>
             <VictoryLabel text="Price per Unit" x={225} y={5} textAnchor="middle" />
